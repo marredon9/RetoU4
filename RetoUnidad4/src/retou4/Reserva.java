d3c98b8651c261b7dda5071a593e351b105de8ff
Unax <189848518+unassembly@users.noreply.github.com>
2025-01-16 17:47:15 +0100

    Creados atributos de la clase reserva -Unax

--- RetoUnidad4/src/retou4/Reserva.java
+++ RetoUnidad4/src/retou4/Reserva.java
@@ -1,5 +1,9 @@
 package retou4;
 
 public class Reserva {
-	
+	static int numero_reserva; //Usada para dar un valor unico a cada id. Sumar 1 cuando se llame al constructor.
+	int id;
+	Usuario usuario;
+	Libro libro;
+	String fecha;
 }
