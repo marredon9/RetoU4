5f4a09a38a29cf7896821f962ddf004a37e15a31
Unax <189848518+unassembly@users.noreply.github.com>
2025-01-16 18:21:53 +0100

    Creada lista de reservas, constructor modificado a privado, creada función crearReserva -Unax

--- RetoUnidad4/src/retou4/Reserva.java
+++ RetoUnidad4/src/retou4/Reserva.java
@@ -2,16 +2,22 @@ package retou4;
 
 public class Reserva {
 	static int numero_reserva; //Usada para dar un valor unico a cada id. Sumar 1 cuando se llame al constructor.
+	static Reserva[] reservas = new Reserva[64];
 	int id;
 	Usuario usuario;
 	Libro libro;
 	String fecha;
 	
-	Reserva(Usuario usuario, Libro libro, String fecha)
+	private Reserva(Usuario usuario, Libro libro, String fecha)
 	{
 		this.id = numero_reserva;
 		numero_reserva++;
 		this.libro = libro;
 		this.fecha = fecha;
+	}
+	
+	static void crearReserva(Usuario usuario, Libro libro)
+	{
+		
 	}
 }
