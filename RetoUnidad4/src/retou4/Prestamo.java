2ca407213d910def608250e8c54ce3e64dbb5a8d
0ier <194371770+0ier@users.noreply.github.com>
2025-01-17 10:26:58 +0100

    Update Prestamo.java

--- RetoUnidad4/src/retou4/Prestamo.java
+++ RetoUnidad4/src/retou4/Prestamo.java
@@ -6,4 +6,13 @@ public class Prestamo {
 	    private Usuario usuario;
 	    private LocalDate fechaPrestamo;
 	    private LocalDate fechaDevolucion;
+	
+public Prestamo(String idPrestamo, Libro libro, Usuario usuario, LocalDate fechaPrestamo, LocalDate fechaDevolucion) {
+        this.idPrestamo = idPrestamo;
+        this.libro = libro;
+        this.usuario = usuario;
+        this.fechaPrestamo = fechaPrestamo;
+        this.fechaDevolucion = fechaDevolucion;
+        this.libro.setDisponible(false);
+    }
 }
