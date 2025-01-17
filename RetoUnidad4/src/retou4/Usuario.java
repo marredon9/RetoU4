a56eeda9e700b2b675141c0a3851d254d9195160
Unax <189848518+unassembly@users.noreply.github.com>
2025-01-17 16:17:26 +0100

    Modificada clase usuario para operar con arrays en vez de ArrayList -Unax

--- RetoUnidad4/src/retou4/Usuario.java
+++ RetoUnidad4/src/retou4/Usuario.java
@@ -1,19 +1,20 @@
 package retou4;
 
-import java.util.*;
+//import java.util.*;
 
 public class Usuario {
 	String nombre;
 	String idUnico;
 	String rol;
-	List<String> librosPrestados;
+	//List<String> librosPrestados;
+	String[] librosPrestados;
 	
 	
 	public Usuario (String nombre, String idUnico, String rol) {
 		this.nombre = nombre;
 		this.idUnico = idUnico;
 		this.rol = rol;
-		this.librosPrestados = new ArrayList<>();
+		this.librosPrestados = new String[10];
 	}
 	
 	public static Usuario registrarUsuario(String nombre, String idUnico, String rol) {
@@ -21,14 +22,14 @@ public class Usuario {
 		
 	}
 	
-	public void listarUsuario(List<Usuario> usuarios) {
+	public void listarUsuario(Usuario[] usuarios) {
 		System.out.println("------Usuarios Registrados:------");
 		 for (Usuario usuario : usuarios) {
 			 System.out.println("Nombre: " + usuario.nombre + "\n" + " ID: " + usuario.idUnico + "\n"  + "Rol: " + usuario.rol);
 		 }
 	}
 	
-	public boolean verificarPrestamo(List<Prestamo> prestamos) {
+	public boolean verificarPrestamo(Prestamo[] prestamos) {
 		int limitePrestamos = 0;
 
 		switch (rol.toLowerCase()) {
