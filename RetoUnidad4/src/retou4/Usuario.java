21a2eac9733e73cf18d219b1a1211beec46ca4ec
Unax <189848518+unassembly@users.noreply.github.com>
2025-01-17 23:23:48 +0100

    Aplicados los cambios de la rama new_develop en la rama main -Unax

--- RetoUnidad4/src/retou4/Usuario.java
+++ RetoUnidad4/src/retou4/Usuario.java
@@ -1,57 +1,87 @@
 package retou4;
 
-import java.util.*;
+//import java.util.*;
 
 public class Usuario {
+	static int cont_ids = 1;
 	String nombre;
-	String idUnico;
+	int idUnico;
 	String rol;
-	List<String> librosPrestados;
+	//List<String> librosPrestados;
+	Prestamo[] librosPrestados;
+	static Usuario[] usuarios = new Usuario[16];
 	
 	
-	public Usuario (String nombre, String idUnico, String rol) {
+	public Usuario (String nombre, int idUnico, String rol, int prestamos) {
 		this.nombre = nombre;
 		this.idUnico = idUnico;
 		this.rol = rol;
-		this.librosPrestados = new ArrayList<>();
+		this.librosPrestados = new Prestamo[prestamos];
 	}
 	
-	public static Usuario registrarUsuario(String nombre, String idUnico, String rol) {
-		return new Usuario (nombre, idUnico, rol);
+	public static void registrarUsuario(String nombre, String rol) {
+		if (usuarios[usuarios.length - 1] != null)
+		{
+			System.out.println("Se ha alcanzado el límite de usuarios a registrar."); //Si el último usuario en la lista no es nulo, no hay huecos libres.
+			return;
+		}
+		int prestamos;
+		switch (rol.toLowerCase())
+		{
+		case "invitado":
+			prestamos = 1;
+			break;
+		case "estudiante":
+			prestamos = 3;
+			break;
+		case "profesor":
+			prestamos = 5;
+			break;
+		default:
+			prestamos = 0;
+			System.err.println("ERROR: Se ha creado un usuario de rol desconocido.");
+		}
+		
+		//Registrar usuario en lista
+		for (int i = 0; i < usuarios.length; i++)
+		{
+			if (usuarios[i] == null)
+			{
+				usuarios[i] = new Usuario(nombre, cont_ids, rol, prestamos);
+				cont_ids++;
+				break;
+			}
+		}
 		
 	}
 	
-	public void listarUsuario(List<Usuario> usuarios) {
-		System.out.println("------Usuarios Registrados:------");
-		 for (Usuario usuario : usuarios) {
-			 System.out.println("Nombre: " + usuario.nombre + "\n" + " ID: " + usuario.idUnico + "\n"  + "Rol: " + usuario.rol);
-		 }
+	public static void listarUsuario() {
+		//System.out.println("------Usuarios Registrados:------");
+		if (usuarios[0] == null)
+		{
+			System.out.println("Actualmente no existe ningún usuario registrado.");
+			return;
+		}
+		
+		for (int i = 0; i < usuarios.length; i++)
+		{
+			if (usuarios[i] == null) return;
+			//contar prestamos disponibles
+			int prestamosDisponibles = 0;
+			for (int j = 0; j < usuarios[i].librosPrestados.length; j++)
+			{
+				if (usuarios[i].librosPrestados[j] == null) prestamosDisponibles++;
+			}
+			System.out.println("Nombre: " + usuarios[i].nombre + ", ID: " + usuarios[i].idUnico + ", Rol: " + usuarios[i].rol + ", Préstamos disponibles: " + prestamosDisponibles);
+		}
 	}
 	
-	public boolean verificarPrestamo(List<Prestamo> prestamos) {
-		int limitePrestamos = 0;
-
-		switch (rol.toLowerCase()) {
-		case "estudiante":
-			limitePrestamos = 3;
-			break;
-		case "profesor":
-			limitePrestamos = 5;
-			break;
-		case "invitado":
-			limitePrestamos = 1;
-			break;
-		default:
-			System.out.println("Rol no valido");
-			return false;
+	public boolean verificarPrestamo() { //devuelve si el usuario puede hacer un prestamo
+		int total = 0;
+		for (int i = 0; i < this.librosPrestados.length; i++)
+		{
+			if (this.librosPrestados[i] == null) total++;
 		}
-
-		int prestamosActuales = 0;
-		for(Prestamo prestamo : prestamos){
-			if(prestamo.getUsuario().equals(this)){
-				prestamosActuales++;
-			}
-		}
-		return prestamosActuales < limitePrestamos;
+		return (total > 0);
 	}
 }
