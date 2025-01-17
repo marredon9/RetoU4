d19fa822b16b036044909f0441fbca8c9d1519ae
Unax <189848518+unassembly@users.noreply.github.com>
2025-01-17 23:10:47 +0100

    Implementada gestión de reservas en el programa principal, se han corregido errores en otras clases. -Unax

--- RetoUnidad4/src/retou4/Principal.java
+++ RetoUnidad4/src/retou4/Principal.java
@@ -5,15 +5,6 @@ import java.util.Scanner;
 public class Principal {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		Libro.registrarLibro("El Lenguaje de Programación C", "Dennis Ritchie", 1798, 10);
-		Libro.registrarLibro("Asi es la Puta Vida", "Jordi Wild", 2022, 5);
-		Libro.registrarLibro("Neon Genesis Evangelion 4", "Yoshiyuki Sadamoto", 2022, 1);
-		
-		Usuario.registrarUsuario("Unax", "Invitado");
-		Usuario.registrarUsuario("Borja", "Profesor");
-		Usuario.registrarUsuario("Marina", "Estudiante");
-		Usuario.registrarUsuario("Oier", "Estudiante");
-		
 		
 		while (true)
 		{	
@@ -169,11 +160,13 @@ public class Principal {
 							}
 							if (busqueda == 0) break;
 						}
+						operacion = 0;
 						break;
 					case 3:
 						//LISTAR LIBROS
 						System.out.println("Libros disponibles:");
 						Libro.mostrarLibros();
+						operacion = 0;
 						break;
 					default:
 						//ERROR
@@ -200,6 +193,11 @@ public class Principal {
 						//REALIZAR PRÉSTAMO
 						Libro prestado;
 						Usuario usuario = null;
+						if (Libro.contarLibros() == 0)
+						{
+							System.out.println("No existen libros para prestar.");
+							break;
+						}
 						while (true)
 						{
 							Libro.mostrarLibros();
@@ -245,6 +243,7 @@ public class Principal {
 						int id_prestamo = Prestamo.cont_ids;
 						Prestamo.iniciarPrestamo(prestado, usuario, fecha);
 						System.out.println("Se ha iniciado un préstamo con el ID " + id_prestamo);
+						operacion = 0;
 						break;
 					case 2:
 						//FINALIZAR PRÉSTAMO
@@ -274,6 +273,7 @@ public class Principal {
 							finalizar_prestamo.finalizarPrestamo(fecha_fin);
 							System.out.println("Se ha finalizado el préstamo.");
 						}
+						operacion = 0;
 						break;
 					default:
 						//ERROR
@@ -286,6 +286,82 @@ public class Principal {
 			case 4:
 				//GESTIÓN DE RESERVAS
 				while (true)
+				{
+					int operacion = getInt(sc, "Introduce la gestión de reservas a realizar\n"
+							+ "[0] Volver.\n"
+							+ "[1] Crear reserva.\n"
+							+ "[2] Listar reservas.\n"
+							+ "[3] Liberar reserva.");
+					switch (operacion)
+					{
+					case 0:
+						break;
+					case 1:
+						//CREAR RESERVA
+						Libro libro;
+						Usuario usuario = null;
+						if (Libro.contarLibros() == 0)
+						{
+							System.out.println("No existen libros para reservar.");
+							break;
+						}
+						while (true)
+						{
+							Libro.mostrarLibros();
+							int id_libro = getInt(sc, "Introduce el ID libro a reservar: ");
+							libro = Libro.consultarLibroPorId(id_libro);
+							if (libro == null) System.out.println("No existe ningún libro con ese ID.");
+							break;
+						}
+						if (libro == null) break;
+						while (true)
+						{
+							Usuario.listarUsuario();
+							int id_usuario = getInt(sc, "Introduce el ID del usuario que va a reservar el libro: ");
+							//buscar usuario
+							for (int i = 0; i < Usuario.usuarios.length; i++)
+							{
+								if (Usuario.usuarios[i] == null) break;
+								if (id_usuario == Usuario.usuarios[i].idUnico)
+								{
+									usuario = Usuario.usuarios[i];
+								}
+							}
+							if (usuario == null)
+							{
+								System.out.println("No existe ningún usuario con ese ID.");
+							}
+							else
+							{
+								break;
+							}
+						}
+						System.out.print("Introduce la fecha de la reserva: ");
+						String fecha = sc.next();
+						Reserva.crearReserva(usuario, libro, fecha);
+						operacion = 0;
+						break;
+					case 2:
+						//LISTAR RESERVAS
+						Reserva.listarReservas();
+						operacion = 0;
+						break;
+					case 3:
+						//LIBERAR RESERVA
+						Reserva.listarReservas();
+						sc.nextLine();
+						int id = getInt(sc, "Introduce el ID de la reserva a liberar: ");
+						Reserva.liberarReserva(operacion);
+						operacion = 0;
+						break;
+					default:
+						//ERROR
+						System.out.println("La opción introducida no es válida.");
+						break;
+					}
+					
+					if (operacion == 0) break;
+				}
 				break;
 			default:
 				System.out.println("Error: La opción elegida no es válida.");
@@ -295,68 +371,7 @@ public class Principal {
 		}
 		System.out.println("Se ha salido del programa. Hasta pronto!");
     }
-
-    /*private static void registrarLibro(Scanner scanner) {
-        int id = Libro.obtenerNumero(scanner, "Ingrese el ID del libro: ");
-        String titulo = Libro.obtenerTexto(scanner, "Ingrese el título del libro: ");
-        String autor = Libro.obtenerTexto(scanner, "Ingrese el autor del libro: ");
-        
-        int año = Libro.obtenerAño(scanner, "Ingrese el año de publicación: ");
-        
-        int cantidad = Libro.obtenerNumero(scanner, "Ingrese la cantidad de copias disponibles: ");
-
-        if (Libro.registrarLibro(id, titulo, autor, año, cantidad)) {
-            System.out.println("Libro registrado con éxito.");
-        }
-    }
-
-
-    private static void consultarLibroPorId(Scanner scanner) {
-        int id = Libro.obtenerNumero(scanner, "Ingrese el ID del libro a consultar: ");
-        Libro libro = Libro.consultarLibroPorId(id);
-        if (libro != null) {
-            System.out.println(libro);
-        } else {
-            System.out.println("Libro no encontrado.");
-        }
-    }
-
-    private static void consultarLibroPorTitulo(Scanner scanner) {
-        String titulo = Libro.obtenerTexto(scanner, "Ingrese el título del libro a consultar: ");
-        Libro libro = Libro.consultarLibroPorTitulo(titulo);
-        if (libro != null) {
-            System.out.println(libro);
-        } else {
-            System.out.println("Libro no encontrado.");
-        }
-    }
-
-    private static void añadirReserva(Scanner scanner) {
-        int id = Libro.obtenerNumero(scanner, "Ingrese el ID del libro para hacer la reserva: ");
-        Libro libro = Libro.consultarLibroPorId(id);
-        if (libro != null) {
-            String nombreReserva = Libro.obtenerTexto(scanner, "Ingrese el nombre para la reserva: ");
-            if (libro.añadirReserva(nombreReserva)) {
-                System.out.println("Reserva añadida correctamente.");
-            }
-        } else {
-            System.out.println("Libro no encontrado.");
-        }
-    }
-
-    private static void eliminarReserva(Scanner scanner) {
-        int id = Libro.obtenerNumero(scanner, "Ingrese el ID del libro para eliminar la reserva: ");
-        Libro libro = Libro.consultarLibroPorId(id);
-        if (libro != null) {
-            String nombreReserva = Libro.obtenerTexto(scanner, "Ingrese el nombre de la reserva a eliminar: ");
-            if (libro.eliminarReserva(nombreReserva)) {
-                System.out.println("Reserva eliminada correctamente.");
-            }
-        } else {
-            System.out.println("Libro no encontrado.");
-        }
-    }*/
-    
+	
     static int getInt(Scanner sc, String prompt)
     {
     	while (true)
