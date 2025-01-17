8761733acf01a6d865c33a21c5936eeeacd677e2
Borja <24dw.borja.villada@arangoya.net>
2025-01-17 12:22:41 +0100

    Se ha creado al estructura inicial de la clase Libro con sus atributos basicos - Borja

--- RetoUnidad4/src/retou4/Libro.java
+++ RetoUnidad4/src/retou4/Libro.java
@@ -1,5 +1,40 @@
 package retou4;
 
 public class Libro {
-	
+    private int id;
+    private String titulo;
+    private String autor;
+    private int añoPublicacion;
+    private int cantidad;
+    private static Libro[] libros = new Libro[10]; 
+    private static int cantidadLibros = 0; 
+
+    // Constructor
+    public Libro(int id, String titulo, String autor, int añoPublicacion, int cantidad) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.añoPublicacion = añoPublicacion;
+        this.cantidad = cantidad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getAñoPublicacion() {
+        return añoPublicacion;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
 }
