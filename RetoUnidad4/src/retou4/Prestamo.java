a07f36b2133214cd16f65e2ce1ede7dc48ecb333
0ier <194371770+0ier@users.noreply.github.com>
2025-01-17 10:25:33 +0100

    Update Prestamo.java

--- RetoUnidad4/src/retou4/Prestamo.java
+++ RetoUnidad4/src/retou4/Prestamo.java
@@ -1,5 +1,9 @@
 package retou4;
-
+import java.time.LocalDate;
 public class Prestamo {
-
+	 private String idPrestamo;
+	    private Libro libro;
+	    private Usuario usuario;
+	    private LocalDate fechaPrestamo;
+	    private LocalDate fechaDevolucion;
 }
