b8943694e0f405ce833d9e26f636ebaef12e3041
0ier <194371770+0ier@users.noreply.github.com>
2025-01-17 11:54:43 +0100

    Update Principal.java

--- RetoUnidad4/src/retou4/Principal.java
+++ RetoUnidad4/src/retou4/Principal.java
@@ -5,3 +5,6 @@ public class SistemaBiblioteca {
 
         biblioteca.agregarLibro(new Libro("1", "El Quijote", "Cervantes"));
         biblioteca.agregarLibro(new Libro("2", "Cien años de soledad", "García Márquez"));
+
+        biblioteca.agregarUsuario(new Usuario("U1", "Juan", "Estudiante"));
+        biblioteca.agregarUsuario(new Usuario("U2", "María", "Profesor"));
