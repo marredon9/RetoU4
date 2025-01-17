066b53903693a9b120f6325712cdbce67cb5bd22
marredon9 <131159001+marredon9@users.noreply.github.com>
2025-01-17 09:40:21 +0100

    Update Usuario.java

--- RetoUnidad4/src/retou4/Usuario.java
+++ RetoUnidad4/src/retou4/Usuario.java
@@ -3,33 +3,6 @@ package retou4;
 import java.util.*;
 
 public class Usuario {
-	String nombre;
-	String idUnico;
-	String rol;
-	List<String> librosPrestados;
-	
-	
-	public Usuario (String nombre, String idUnico, String rol) {
-		this.nombre = nombre;
-		this.idUnico = idUnico;
-		this.rol = rol;
-		this.librosPrestados = new ArrayList<>();
-	}
-	
-	public static Usuario registrarUsuario(String nombre, String idUnico, String rol) {
-		return new Usuario (nombre, idUnico, rol);
-		
-	}
-	
-	public void listarUsuario(List<Usuario> usuarios) {
-		System.out.println("------Usuarios Registrados:------");
-		 for (Usuario usuario : usuarios) {
-			 System.out.println("Nombre: " + usuario.nombre + "\n" + " ID: " + usuario.idUnico + "\n"  + "Rol: " + usuario.rol);
-		 }
-	}
-	
-	public void verificarPrestamo() {
-		
-	}
+
 }
 
