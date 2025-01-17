9717d87e70f02bfa13abafab0dd6c7347729a227
0ier <194371770+0ier@users.noreply.github.com>
2025-01-17 10:38:50 +0100

    Update Prestamo.java

--- RetoUnidad4/src/retou4/Prestamo.java
+++ RetoUnidad4/src/retou4/Prestamo.java
@@ -1,5 +1,38 @@
 package retou4;
-
+import java.time.LocalDate;
 public class Prestamo {
+	 private String idPrestamo;
+	    private Libro libro;
+	    private Usuario usuario;
+	    private LocalDate fechaPrestamo;
+	    private LocalDate fechaDevolucion;
 	
+public Prestamo(String idPrestamo, Libro libro, Usuario usuario, LocalDate fechaPrestamo, LocalDate fechaDevolucion) {
+        this.idPrestamo = idPrestamo;
+        this.libro = libro;
+        this.usuario = usuario;
+        this.fechaPrestamo = fechaPrestamo;
+        this.fechaDevolucion = fechaDevolucion;
+        this.libro.setDisponible(false);
+    }
 }
+
+public String getIdPrestamo() {
+        return idPrestamo;
+    }
+
+    public Libro getLibro() {
+        return libro;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public LocalDate getFechaPrestamo() {
+        return fechaPrestamo;
+    }
+
+    public LocalDate getFechaDevolucion() {
+        return fechaDevolucion;
+    }
