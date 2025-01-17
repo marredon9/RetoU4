eaceedd390853bba14261c0f4331f152217d0c68
0ier <194371770+0ier@users.noreply.github.com>
2025-01-17 11:54:22 +0100

    Update Principal.java

--- RetoUnidad4/src/retou4/Principal.java
+++ RetoUnidad4/src/retou4/Principal.java
@@ -2,3 +2,6 @@ package retou4;
 public class SistemaBiblioteca {
     public static void main(String[] args) {
         Biblioteca biblioteca = new Biblioteca();
+
+        biblioteca.agregarLibro(new Libro("1", "El Quijote", "Cervantes"));
+        biblioteca.agregarLibro(new Libro("2", "Cien años de soledad", "García Márquez"));
